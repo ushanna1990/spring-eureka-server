bb0181a9d5f154cbb101e39d0a76970437a2e66a
ushanna peddapelliwar <peddapelliwarushanna@gmail.com>
2025-12-04 23:00:11 +0530

    Initial commit

---
+++ src/main/java/com/ncp/eureka/NcpEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ncp.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcpEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcpEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncp/eureka/NcpEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncp.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcpEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
